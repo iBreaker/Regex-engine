570703bddb924a2ab182a2e52d866ca4d3128f8c
breaker <791628659@qq.com>
2015-12-13 22:42:53 +0800

    修改格式错误

--- ac.c
+++ ac.c
@@ -1,5 +1,6 @@
-/*AC自动机算法
-自动机算法可以参考《柔性字符串匹配》里的相应章节，讲的很清楚
+/*
+    AC自动机算法
+    自动机算法可以参考《柔性字符串匹配》里的相应章节，讲的很清楚
 */
 
 #include <stdio.h>
@@ -8,12 +9,14 @@
 const  int MAXQ = 500000+10;
 const  int MAXN = 1000000+10;
 const  int MAXK = 26;  //自动机里字符集的大小 
+
 struct  TrieNode
 {
-	TrieNode* fail;
+	TrieNode* fail;         // 
 	TrieNode* next[MAXK];
-	bool danger;   //该节点是否为某模式串的终结点 
-	int  cnt;    //以该节点为终结点的模式串个数 
+	bool danger;            // 该节点是否为某模式串的终结点 
+	int  cnt;               // 以该节点为终结点的模式串个数
+	
 	TrieNode()
 	{
 		fail = NULL;
@@ -21,17 +24,20 @@ struct  TrieNode
 		danger = false;
 		cnt = 0;
 	}
+	
 }*que[MAXQ], *root;
+
 //文本字符串
 char  msg[MAXN];
 int   N;
-void  TrieInsert(char *s)
+
+void  TrieInsert(char *s)   //插入新的模式串
 {
 	int  i = 0;
 	TrieNode *ptr = root;
-	while(s<span style="text-decoration: line-through;">)
+	while(s)
 	{
-		int  idx = s<span style="text-decoration: line-through;">-'a';
+		int idx = s - 'a';
 		if(ptr->next[idx] == NULL)
 			ptr->next[idx] = new TrieNode();
 		ptr = ptr->next[idx];
@@ -41,23 +47,22 @@ void  TrieInsert(char *s)
 	ptr->cnt++;
 }
 
-void  Init()
+void  Init()    // 
 {
 	int  i;
 	char  s[100];
-	root = new TrieNode();
-	
+	root = new TrieNode();      // 创建root
 	printf("输入模式串数量：");
 	scanf("%d", &N);
 	for(i = 0; i < N; i++)
 	{
 		printf("输入第%d个模式串（共%d个）：",i,N);
 		scanf("%s", s);
-		TrieInsert(s);
+		TrieInsert(s);         // 插入新的模式串
 	}
 }
 
-void  Build_AC_Automation()
+void  Build_AC_Automation()     // 创建ac状态机
 {
 	int  rear = 1, front = 0, i;
 	que[0] = root;
@@ -66,37 +71,38 @@ void  Build_AC_Automation()
 	{
 		TrieNode *cur = que[front++];
 		for(i = 0; i < 26; i++)
-			if(cur->next<span style="text-decoration: line-through;"> != NULL)
+			if(cur->next != NULL)
 			{
 				if(cur == root)
-					cur->next<span style="text-decoration: line-through;">->fail = root;
+					cur->next->fail = root;
 				else
 				{
 					TrieNode *ptr = cur->fail;
 					while(ptr != NULL)
 					{
-						if(ptr->next<span style="text-decoration: line-through;"> != NULL)
+						if(ptr->next != NULL)
 						{
-							cur->next<span style="text-decoration: line-through;">->fail = ptr->next<span style="text-decoration: line-through;">;
-							if(ptr->next<span style="text-decoration: line-through;">->danger == true)
-								cur->next<span style="text-decoration: line-through;">->danger = true;
+							cur->next->fail = ptr->next;
+							if(ptr->next->danger == true)
+								cur->next->danger = true;
 							break;
 						}
 						ptr = ptr->fail;
 					}
-					if(ptr == NULL) cur->next<span style="text-decoration: line-through;">->fail = root;
+					if(ptr == NULL) cur->next->fail = root;
 				}
-				que[rear++] = cur->next<span style="text-decoration: line-through;">;
+				que[rear++] = cur->next;
 			}
 	}
 }
-int  AC_Search()
+
+int  AC_Search()    // ac查找
 {
 	int  i = 0, ans = 0;
 	TrieNode *ptr = root;
-	while(msg<span style="text-decoration: line-through;">)
+	while(msg)
 	{
-		int  idx = msg<span style="text-decoration: line-through;">-'a';
+		int  idx = msg-'a';
 		while(ptr->next[idx] == NULL && ptr != root) ptr = ptr->fail;
 		ptr = ptr->next[idx];
 		if(ptr == NULL) ptr = root;
@@ -111,14 +117,15 @@ int  AC_Search()
 	}
 	return  ans;
 }
-int  main()
+
+int  main()     // 主函数
 {
 	int  T;
 	printf("输入测试次数：");
 	scanf("%d", &T);
 	while(T--)
 	{
-		Init();
+		Init();  // 初始化
 		Build_AC_Automation();
 		//文本 
 		printf("输入匹配文本：");
