4434facf1b26b8a8578af21f78532ee2baf9dbf2
Breaker <791628659@qq.com>
2015-12-19 17:57:26 +0800

    py

---
+++ ac.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#coding:utf-8
+
+class AC(object):
+    'AC 算法类'
+
+    def __init__(self, patterns, string):
+        pass
+
+    def CreateGoto(self ):
+        pass
+
+    def CreateFailure(self ):
+        pass
+
+    def CreateOut(self ):
+        pass
+
+    def search(self ):
+        pass
+
+def test():
+    patterns = ['he', 'she', 'his', 'hers']
+    string = 'heeessisdsdfhhddihers'
+
+    ac = AC(patterns, string)
+    ac.search()
+
+
+if __name__ == "__main__":
+    test()
+
