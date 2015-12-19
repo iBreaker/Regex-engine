60421fda4bb9710d121e3a4653144ffa0ac8afbc
Breaker <791628659@qq.com>
2015-12-19 23:26:02 +0800

    写到一半

--- ac.py
+++ ac.py
@@ -1,32 +1,70 @@
 #!/usr/bin/python
-#coding:utf-8
+# coding:utf-8
+
+
+import os
+import logging
+
+
+class Node(object):
+    """节点"""
+
+    def __init__(self, ggoto=dict(), failure=0, out=''):
+        self.ggoto = ggoto
+        self.failure = failure
+        self.out = out
+
 
 class AC(object):
-    'AC 算法类'
+    """ AC 算法类 """
 
-    def __init__(self, patterns, string):
+    def __init__(self, patterns):
+        self.patterns = patterns
+        self.string = ''
+        self.root = Node()
         pass
 
-    def CreateGoto(self ):
+    def create_goto(self):
+        """ 创建goto表 """
+
+        current = self.root
+        for pattern in self.patterns:
+            for char in pattern:
+                logging.debug("func=create_goto current char is:%c", char)
+                if(not current.ggoto.has_key(char)):
+                    logging.debug("func=create_goto need to create a new key:%c", char)
+                    new_node = Node()
+                    current.ggoto[char] = new_node
+                    current = new_node
+                else:
+                    current = current.ggoto[char]
+            current = self.root
+            pass
+
+
+
+
+    def create_failure(self):
         pass
 
-    def CreateFailure(self ):
+    def create_out(self):
         pass
 
-    def CreateOut(self ):
+    def search(self, string):
+        self.string = string
         pass
 
-    def search(self ):
-        pass
 
 def test():
     patterns = ['he', 'she', 'his', 'hers']
     string = 'heeessisdsdfhhddihers'
 
-    ac = AC(patterns, string)
-    ac.search()
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s:%(levelname)s:%(message)s')
+    ac = AC(patterns)
+    ac.search(string)
+    ac.create_goto()
 
 
 if __name__ == "__main__":
     test()
-
